024237a7c9c745cb967dd83d84663a6bafaed84f
Raj Rajhans <raj.rajhans18@vit.edu>
2019-07-28 02:05:39 +0530

    First commit, set up create-react-app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class FileList extends React.Component{
+    constructor(props){
+        super(props)
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    render(){
+        return(
+            <table className={"file-list"}>
+
+            </table>
+        )
+    }
+}
